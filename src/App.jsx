b1703867398f2f5d1221115fc17361b44dd252ca
vanessitta <vanessafreitasferreir4@gmail.com>
2025-07-30 13:43:50 -0400

    Corrige importação do componente Contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import Sobre from "./components/Sobre";
 import Tech from "./components/Tech";
 import Projects from "./components/Projects";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 export default function App() {
